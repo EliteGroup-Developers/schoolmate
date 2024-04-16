b860e5cd0d4165de013e27439de80e6420c7bb55
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-16 18:47:50 +0100

    changes

---
+++ clientui/src/DashBoard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import DashBoardNav from './DashBoardNav'
+
+const DashBoard = () => {
+  return (
+    // <section className=''>
+  <DashBoardNav/>
+    
+    
+    // </section>
+  )
+}
+
+export default DashBoard
